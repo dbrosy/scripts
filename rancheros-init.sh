7910143d086c2eb9a50f54a74ea97466aab44426
dbrosy <dbrosy@hotmail.com>
2018-02-12 12:26:39 +1100

    Create rancheros-init.sh

---
+++ rancheros-init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -ex
+exec &>> /var/log/install.log
+
+if blkid | grep RANCHER_STATE; then
+	# don't re-format
+	echo "DONE"
+	exit
+else
+  echo "RancherOS not found"
+fi
+
+INSTALL_DISK="/dev/vda"
+if ! fdisk -l $INSTALL_DISK; then
+	INSTALL_DISK="/dev/sda"
+  echo "RancherOS Installdisk - /dev/sda"
+else
+  echo "RancherOS Installdisk - /dev/vda"
+fi
+
