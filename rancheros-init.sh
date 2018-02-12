ac165579786687ed4bd5d731632efcba51b7c915
dbrosy <dbrosy@hotmail.com>
2018-02-12 13:01:58 +1100

    Update rancheros-init.sh

--- rancheros-init.sh
+++ rancheros-init.sh
@@ -9,7 +9,7 @@ else
 fi
 
 INSTALL_DISK="/dev/vda"
-if ! fdisk -l $INSTALL_DISK > /dev/null 2/&1; then
+if ! ( fdisk -l ${INSTALL_DISK} ); then
 	INSTALL_DISK="/dev/sda"
   echo "RancherOS Installdisk - /dev/sda"
 else
