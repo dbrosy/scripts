35322a2b79a81eaeeec3da17361851dd82c8ac63
dbrosy <dbrosy@hotmail.com>
2018-09-11 18:32:32 +1000

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -46,7 +46,12 @@ else
   sudo usermod -aG docker $USER
 
   echo "Installer: Installing Docker-Compose
-  sudo "curl -L https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose"
+  COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1`
+  sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+  sudo chmod +x /usr/local/bin/docker-compose
+  sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+  
+  # sudo "curl -L https://github.com/docker/compose/releases/download/$COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose"
   sudo chmod +x /usr/local/bin/docker-compose
 fi
 
