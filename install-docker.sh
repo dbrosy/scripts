e70618301ca066e7ba3d173697f170467cb83732
dbrosy <dbrosy@hotmail.com>
2018-09-11 18:50:15 +1000

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -47,6 +47,9 @@ else
   sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
   sudo chmod +x /usr/local/bin/docker-compose
   sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
+  clear
+  docker -v
+  docker-compose -v
 fi
 
 exit
